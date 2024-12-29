0d2c07719e183880eb7d301a8d5671cf35677640
Alfredo López <alfredolopezavelar1@gmail.com>
2024-12-28 22:45:03 -0600

    Added user models, admin interace for models, and GUI documentation

--- app/core/models.py
+++ app/core/models.py
@@ -11,10 +11,24 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     """Manager for users"""
     
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
 
+        return user 
+    
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
@@ -26,5 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
